9fd732d76b43c7b7d1b1316bb9e0e010a002107c
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-10 14:49:06 +0100

    Added exercise 2 - Finding the index position of an element

--- src/main/java/lexicon/exercises/Main.java
+++ src/main/java/lexicon/exercises/Main.java
@@ -6,10 +6,22 @@ public class Main {
 
         /*1. Write a program which will store elements in an array of type `int` and print it out.
            Expected output: `11 23 39` etc.*/
-        int a[] = {11,23,39};
-        for(int x = 0 ; x < 3 ; x++){
-            System.out.print(a[x]);
-            System.out.print("  ");
+        int abc[] = {11,23,39};
+        for(int x = 0 ; x < 3 ; x++) {
+            System.out.print(abc[x] + "  ");
+        }
+        System.out.println();
+
+        /*2. Create a program and create a method with name `indexOf` which will find and return the index of an element in the
+            array. If the element doesn’t exist your method should return `-1` as value.
+            Expected output: `Index position of number 5 is: 2`*/
+            int number[] = {1,2,3,4,5,6,7};
+            indexOf(number);
+    }
+
+    static void indexOf(int number[]){
+        for (int a = 0; a < number.length ; a++){
+            System.out.println("Index position of number "+number[a]+" is: "+a);
         }
     }
 }
