2b7d8602c74fb722808c6091d252e588118c1828
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-10 14:27:40 +0100

    Added exercise 1 - Print int numbers from Arrays

--- src/main/java/lexicon/exercises/Main.java
+++ src/main/java/lexicon/exercises/Main.java
@@ -1,7 +1,15 @@
 package lexicon.exercises;
 
 public class Main {
+
     static void main() {
 
+        /*1. Write a program which will store elements in an array of type `int` and print it out.
+           Expected output: `11 23 39` etc.*/
+        int a[] = {11,23,39};
+        for(int x = 0 ; x < 3 ; x++){
+            System.out.print(a[x]);
+            System.out.print("  ");
+        }
     }
 }
